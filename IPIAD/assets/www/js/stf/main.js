e9bcd73df9a7ed1db484466e3364c3a7e8fd8166
Ezio Dotti <ezio.dotti@studiofarma.it>
2013-05-07 15:57:08 +0200

    Aggiornamento

--- IPIAD/assets/www/js/stf/main.js
+++ IPIAD/assets/www/js/stf/main.js
@@ -6,7 +6,17 @@ document.addEventListener("deviceready", onDeviceReady, false);
 // Cordova is ready
 //
 function onDeviceReady() {
+    
+
     $(".BtnEsci").click(function () {
         navigator.app.exitApp();
+    });
+    
+
+    $.mobile.loading('show', {
+        text: 'Sto caricando la tua posizione. Attendi !!!',
+        textVisible: true,
+        theme: 'z',
+        html: ""
     });
 }
